fb15e1d0435a4fc45a742096554db072b179489e
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-06-27 07:58:49 +0700

    feat: layout product page

--- apps/web/constant/index.ts
+++ apps/web/constant/index.ts
@@ -1,7 +1,7 @@
 //NAVIGATION
 export const NAV_LINKS = [
   { href: '/', key: 'home', label: 'Home' },
-  { href: '/', key: 'product', label: 'Product' },
+  { href: '/products', key: 'product', label: 'Product' },
   { href: '/', key: 'custom', label: 'Custom' },
   {
     href: '/',
@@ -97,4 +97,18 @@ export const PATERA_STORE = [
       'Gg. Ledok Tukangan Blok DN 2 No.243, RT.02/RW.01, Tegal Panggung, Kec. Danurejan, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55212',
     contact: '0821-1001-2575',
   },
+];
+
+//PRODUCTS
+export const SAMPLE_PRODUCT = [
+  { image: '/Collection/Vase_Cup.jpg', name: 'Choco Milk' },
+  { image: '/Collection/Vase_Cup.jpg', name: 'Choco Milk' },
+  { image: '/Collection/Vase_Cup.jpg', name: 'Choco Milk' },
+  { image: '/Collection/Vase_Cup.jpg', name: 'Choco Milk' },
+  { image: '/Collection/Vase_Cup.jpg', name: 'Choco Milk' },
+];
+
+//SAMPLE PRODUCT ID
+export const SAMPLE_PRODUCT_ID = [
+  { image: '/Collection/Vase_Cup.jpg', name: 'Vase Cup' },
 ];
